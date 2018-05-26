156b24ac832ec6f8e085e2cdf6befd1494d158e8
vyrtualsynthese <12Merde21yuki>
2018-05-26 23:57:49 +0200

    tmi connector connected

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     },
     "extends": "standard",
     "rules": {
-        "indent": ["error", 4],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "semi": ["error", "always"]
     }
 };
