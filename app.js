032eb14886e334d0b4ea1dabe9b0aef7e1692fad
Thomas Talbot <thomas.talbot@fauya.fr>
2018-05-26 23:54:05 +0200

    refactoring Connectors

--- app.js
+++ app.js
@@ -3,15 +3,11 @@
 require('dotenv').load();
 
 const ConnectorIO = require("./lib/Connector/ConnectorIO");
-const StandardConnectorOutput = require("./lib/Connector/StandardConnectorOutput");
-const StandardConnectorInput = require("./lib/Connector/StandardConnectorInput");
+const StandardConnectorIO = require("./lib/Connector/StandardConnectorIO");
 const ConnectorManager = require("./lib/Connector/ConnectorManager");
 const CommandHandler = require("./lib/CommandHandler");
 
-const sci = new StandardConnectorInput();
-const sco = new StandardConnectorOutput();
-
 const connectorManager = new ConnectorManager();
-connectorManager.addConnector(sci);
+connectorManager.addConnector(new StandardConnectorIO());
 
 const commandHandler = new CommandHandler(connectorManager);
