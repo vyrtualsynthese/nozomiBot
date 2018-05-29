54c6765626710767f1ca0519731dab9a3b8bb8a7
Thomas Talbot <th0m45.talbot@gmail.com>
2018-05-29 20:40:29 +0200

    add "command list" + refactoring all commands "command ..."

--- app.js
+++ app.js
@@ -16,7 +16,10 @@
     const CommandHandler = require("./lib/Command/CommandHandler");
     const EchoCommand = require("./lib/Command/EchoCommand");
     const OnlyStandardCommand = require("./lib/Command/OnlyStandardCommand");
-    const CreateStaticCommandCommand = require("./lib/Command/CreateStaticCommandCommand");
+    const CreateStaticCommandCommand = require("./lib/Command/Commands/CreateStaticCommandCommand");
+    const ListStaticCommandsCommand = require("./lib/Command/Commands/ListStaticCommandsCommand");
+    const ListCommandsCommand = require("./lib/Command/Commands/ListCommandsCommand");
+    const CommandsCommand = require("./lib/Command/Commands/CommandsCommand");
     const RandomCommand = require("./lib/Command/RandomCommand");
     const TwitchConnectorIO = require('./lib/Connector/TwitchConnectorIO');
     const StaticCommandRepository = require('./lib/Database/StaticCommandRepository');
@@ -36,10 +39,13 @@
     connectorManager.addConnector(tcio);
 
     const commandHandler = new CommandHandler(connectorManager, staticCommandRepo, logger);
+    commandHandler.registerCommand(new RandomCommand(logger));
     commandHandler.registerCommand(new EchoCommand(logger));
     commandHandler.registerCommand(new OnlyStandardCommand(logger));
+    commandHandler.registerCommand(new ListStaticCommandsCommand(logger, staticCommandRepo));
+    commandHandler.registerCommand(new ListCommandsCommand(logger, staticCommandRepo, commandHandler));
     commandHandler.registerCommand(new CreateStaticCommandCommand(logger, staticCommandRepo));
-    commandHandler.registerCommand(new RandomCommand(logger));
+    commandHandler.registerCommand(new CommandsCommand(logger));
 
     console.log(`Nozomibot is running... command "${scio.exitCommand}" for quit.`);
 
