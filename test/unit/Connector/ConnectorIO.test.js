db61681fe4ab617e86213ceb4d051f668edd80ff
RONTEIX <vyrtualsynthese@gmail.com>
2018-06-01 17:55:50 +0200

    fixed eslint linting coverage file. Addin .eslintignore for better linting management. Writting some more tests and adding getters in any class initiating with variable for proper testing purposes

--- test/unit/Connector/ConnectorIO.test.js
+++ test/unit/Connector/ConnectorIO.test.js
@@ -2,12 +2,17 @@ process.env.NODE_ENV = 'test';
 
 require('chai').should();
 const ConnectorIO = require('../../../lib/Connector/ConnectorIO');
-
-
+const Connector = new ConnectorIO();
 
 describe('Unit: ConnectorIO', () => {
+    describe('setManager', () => {
+        it('should set [connectorManager] to the same as [input]', () => {
+            Connector.setManager('toto')
+            Connector.getManager().should.be.equal('toto');
+        });
+    });
     it('should return and array of two', () => {
         let message = '!tata titi toto';
-        ConnectorIO.parseCommand(message);
+        ConnectorIO.parseCommand(message).should.be.an('object');
     });
 });
