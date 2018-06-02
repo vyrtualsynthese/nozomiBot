6c381b76829ab8abddc062c2d161b109d7ce19d5
vyrtualsynthese <vyrtualsynthese@gmail.com>
2018-06-02 16:13:07 +0200

    broken test

--- test/unit/Connector/ConnectorIO.test.js
+++ test/unit/Connector/ConnectorIO.test.js
@@ -6,12 +6,6 @@ const ConnectorIO = require('../../../lib/Connector/ConnectorIO');
 const Connector = new ConnectorIO();
 
 describe('Unit: ConnectorIO', () => {
-    describe('setManager', () => {
-        it('should set [connectorManager] to the same as [input]', () => {
-            Connector.setManager('toto')
-            Connector.getManager().should.be.equal('toto');
-        });
-    });
     it('should return and array of two', () => {
         ConnectorIO.parseCommand('!tata titi toto').should.be.an('object');
     });
