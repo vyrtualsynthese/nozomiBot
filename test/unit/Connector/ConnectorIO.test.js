a116ee522e478b3393e6e8eaeff97de25c745070
vyrtualsynthese <vyrtualsynthese@gmail.com>
2018-06-02 16:07:20 +0200

    tests and refactor

--- test/unit/Connector/ConnectorIO.test.js
+++ test/unit/Connector/ConnectorIO.test.js
@@ -2,17 +2,9 @@ process.env.NODE_ENV = 'test';
 
 require('chai').should();
 const ConnectorIO = require('../../../lib/Connector/ConnectorIO');
-const Connector = new ConnectorIO();
 
 describe('Unit: ConnectorIO', () => {
-    describe('setManager', () => {
-        it('should set [connectorManager] to the same as [input]', () => {
-            Connector.setManager('toto')
-            Connector.getManager().should.be.equal('toto');
-        });
-    });
     it('should return and array of two', () => {
-        let message = '!tata titi toto';
-        ConnectorIO.parseCommand(message).should.be.an('object');
+        ConnectorIO.parseCommand('!tata titi toto').should.be.an('object');
     });
 });
